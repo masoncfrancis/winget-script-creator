06522ac5fa558ae7a062b8ec77b21df51b1d07d2
Mason Francis <masoncfrancis@gmail.com>
2023-08-14 12:22:38 -0600

    Added argument handling

--- src/main.py
+++ src/main.py
@@ -1,8 +1,45 @@
 # This program was written by ChatGPT
 
 import os
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description="Generate PowerShell script and use winget based on input.")
+    parser.add_argument("-a", "--add", metavar="filename", help="Add program names from a text file and generate a new script")
+    parser.add_argument("-s", "--script", metavar="filename", help="Generate a script based on a text file")
+
+    args = parser.parse_args()
+
+    if args.add:
+        add_programs_from_file(args.add)
+    elif args.script:
+        generate_powershell_script_from_file(args.script)
+    else:
+        interactive_input()
+
+def add_programs_from_file(filename):
+    program_list = []
+    with open(filename, "r") as file:
+        for line in file:
+            program_name = line.strip()
+            if program_name:
+                program_list.append(program_name)
+    if program_list:
+        generate_powershell_script(program_list)
+        print("PowerShell script has been generated based on the added programs.")
+
+def generate_powershell_script_from_file(filename):
+    program_list = []
+    with open(filename, "r") as file:
+        for line in file:
+            program_name = line.strip()
+            if program_name:
+                program_list.append(program_name)
+    if program_list:
+        generate_powershell_script(program_list)
+        print("PowerShell script has been generated based on the provided program list.")
+
+def interactive_input():
     program_list = []
 
     while True:
@@ -19,8 +56,7 @@ def main():
 
 def generate_powershell_script(program_list):
     script_path = "./out/install_apps.ps1"
-    winget_list_path = "./out/winget_list.txt"
-    
+
     with open(script_path, "w") as script_file:
         script_file.write("$programs = @(\n")
         for program in program_list:
